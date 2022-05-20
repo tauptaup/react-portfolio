1cad6a053c922664192d9ab2dbcd7ba3ace82bb9
tauptaup <lxde@pop-os.localdomain>
2022-05-20 13:32:03 +0600

    Updates

---
+++ precache-manifest.21779542e815cd6538e056e9c03d7b6e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220d3c4cf6c4b9ef51efed5d5f908ad2",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0beb378cdb309f1b175d",
+    "url": "/react-portfolio/static/css/main.71225de5.chunk.css"
+  },
+  {
+    "revision": "f02430fdc9ed96e55fbb",
+    "url": "/react-portfolio/static/js/2.088ac3cd.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/react-portfolio/static/js/2.088ac3cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0beb378cdb309f1b175d",
+    "url": "/react-portfolio/static/js/main.622c4b59.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "caeb0f83722a5eb60bc55a61e059f7cd",
+    "url": "/react-portfolio/static/media/avatar.caeb0f83.png"
+  },
+  {
+    "revision": "ce7b449cc55b96e90c36abf0b8b20869",
+    "url": "/react-portfolio/static/media/bg.ce7b449c.jpg"
+  },
+  {
+    "revision": "cc15c848d3affaaf103fede851caf5c4",
+    "url": "/react-portfolio/static/media/discord.cc15c848.jpg"
+  },
+  {
+    "revision": "cd842098258de5647f22032bb34eb6e9",
+    "url": "/react-portfolio/static/media/flutter-fest.cd842098.jpg"
+  },
+  {
+    "revision": "9064847c525eff6b1b7d2fb200cff1d0",
+    "url": "/react-portfolio/static/media/portfolio.9064847c.jpg"
+  }
+]);
